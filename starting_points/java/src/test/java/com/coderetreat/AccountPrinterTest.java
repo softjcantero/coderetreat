d52bf9d32dd6e443cf7412a25a366d830c4adff7
Jesús Cantero Ferrón <soft.jcantero@iberia.es>
2023-06-08 00:15:51 +0200

    Red AccountPrinter test for one deposite

--- starting_points/java/src/test/java/com/coderetreat/AccountPrinterTest.java
+++ starting_points/java/src/test/java/com/coderetreat/AccountPrinterTest.java
@@ -1,16 +1,24 @@
 package com.coderetreat;
 
 import static org.fest.assertions.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+import java.util.Arrays;
+import java.util.Date;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mockito;
 
 public class AccountPrinterTest {
 
     private AccountPrinter accountPrinter;
 
+    private Account account;
+
     @Before
     public void setUp() {
-        accountPrinter = new AccountPrinter();
+        account = Mockito.mock(Account.class);
+
+        accountPrinter = new AccountPrinter(account);
     }
 
     @Test
@@ -18,6 +26,17 @@ public class AccountPrinterTest {
        String statement = accountPrinter.printStatement();
 
        assertThat(statement.equals("Date\t\tAmount\t\tBalance")).isEqualTo(true);
+    }
+
+    @Test
+    public void testOneDeposite() {
+       when(account.getEntries()).thenReturn(Arrays.asList(new AccountEntry(new Date(), 500, 500)));
+
+       String statement = accountPrinter.printStatement();
+
+       assertThat(statement.contains("Date\t\tAmount\t\tBalance\n")).isEqualTo(true);
+       assertThat(statement.contains("+500")).isEqualTo(true);
+       assertThat(statement.contains("500")).isEqualTo(true);
     }
 
 }
