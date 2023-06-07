26d4dab833ac925211baba0336796ff692d88511
Jesús Cantero Ferrón <soft.jcantero@iberia.es>
2023-06-08 01:02:24 +0200

    Green and Refactor one deposit and one withdraw

--- starting_points/java/src/main/java/com/coderetreat/AccountPrinter.java
+++ starting_points/java/src/main/java/com/coderetreat/AccountPrinter.java
@@ -1,13 +1,33 @@
 package com.coderetreat;
 
+import java.text.SimpleDateFormat;
+
 public class AccountPrinter {
 
+    private final SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
 
+    private final Account account;
+
+    public AccountPrinter(Account account) {
+        this.account = account;
+    }
 
     public String printStatement() {
-        StringBuilder sb = new StringBuilder("Date\t\tAmount\t\tBalance");
+        StringBuilder sb = new StringBuilder("Date\t\tAmount\t\tBalance\n");
+
+        for (AccountEntry entry : account.getEntries()) {
+            sb.append(dateFormat.format(entry.getDate())).append("\t\t");
+            sb.append(buildSign(entry.getAmount())).append(entry.getAmount()).append("\t\t");
+            sb.append(entry.getBalance()).append("\t\t");
+        }
+
 
         return sb.toString();
+    }
+
+
+    private Character buildSign(Integer value) {
+        return value > -1 ? '+' : '-';
     }
 
 }
--- starting_points/java/src/test/java/com/coderetreat/AccountPrinterTest.java
+++ starting_points/java/src/test/java/com/coderetreat/AccountPrinterTest.java
@@ -6,18 +6,20 @@ import java.util.Arrays;
 import java.util.Date;
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.Mockito;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
 
+@RunWith(MockitoJUnitRunner.class)
 public class AccountPrinterTest {
 
     private AccountPrinter accountPrinter;
 
+    @Mock
     private Account account;
 
     @Before
     public void setUp() {
-        account = Mockito.mock(Account.class);
-
         accountPrinter = new AccountPrinter(account);
     }
 
@@ -25,7 +27,7 @@ public class AccountPrinterTest {
     public void testHeader() {
        String statement = accountPrinter.printStatement();
 
-       assertThat(statement.equals("Date\t\tAmount\t\tBalance")).isEqualTo(true);
+       assertThat(statement.equals("Date\t\tAmount\t\tBalance\n")).isEqualTo(true);
     }
 
     @Test
@@ -37,6 +39,22 @@ public class AccountPrinterTest {
        assertThat(statement.contains("Date\t\tAmount\t\tBalance\n")).isEqualTo(true);
        assertThat(statement.contains("+500")).isEqualTo(true);
        assertThat(statement.contains("500")).isEqualTo(true);
+    }
+
+    @Test
+    public void testOneDepositeAndOneWithdraw() {
+       when(account.getEntries()).thenReturn(
+           Arrays.asList(new AccountEntry(new Date(), 500, 500), new AccountEntry(new Date(), -200, 300) ));
+
+       String statement = accountPrinter.printStatement();
+
+       assertThat(statement.contains("Date\t\tAmount\t\tBalance\n")).isEqualTo(true);
+       assertThat(statement.contains("+500")).isEqualTo(true);
+       assertThat(statement.contains("500")).isEqualTo(true);
+
+       assertThat(statement.contains("Date\t\tAmount\t\tBalance\n")).isEqualTo(true);
+       assertThat(statement.contains("-200")).isEqualTo(true);
+       assertThat(statement.contains("300")).isEqualTo(true);
     }
 
 }
